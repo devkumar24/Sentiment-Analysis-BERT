6fd78e67d349f631899649c2900a999e84bd547b
DEV KUMAR <59363213+devkumar24@users.noreply.github.com>
2021-08-13 14:33:31 +0530

    updated utils.py

---
+++ utils.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import time
+import json
+import tensorflow as tf
+
+from transformers import TFBertForSequenceClassification
+from transformers import BertTokenizer
+from transformers import InputExample, InputFeatures
+
+
+def create_data_to_example(data,Data_column,Label_column):
+  examples = data.apply(lambda x : InputExample(guid = None,
+                                text_a = x[Data_column],
+                                text_b = None,
+                                label = x[Label_column]), axis = 1
+                          )
+
+  return examples
+
+
+def convert_examples_to_tf_dataset(examples, tokenizer, max_length=128):
+    features = [] # -> will hold InputFeatures to be converted later
+
+    for e in examples:
+        # Documentation is really strong for this method, so please take a look at it
+        input_dict = tokenizer.encode_plus(
+            e.text_a,
+            add_special_tokens=True,
+            max_length=max_length, # truncates if len(s) > max_length
+            return_token_type_ids=True,
+            return_attention_mask=True,
+            pad_to_max_length=True, # pads to the right by default # CHECK THIS for pad_to_max_length
+            truncation=True
+        )
+
+        input_ids, token_type_ids, attention_mask = (input_dict["input_ids"],
+            input_dict["token_type_ids"], input_dict['attention_mask'])
+
+        features.append(
+            InputFeatures(
+                input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, label=e.label
+            )
+        )
+    def gen():
+        for f in features:
+            yield (
+                {
+                    "input_ids": f.input_ids,
+                    "attention_mask": f.attention_mask,
+                    "token_type_ids": f.token_type_ids,
+                },
+                f.label,
+            )
+
+    return tf.data.Dataset.from_generator(
+        gen,
+        ({"input_ids": tf.int32, "attention_mask": tf.int32, "token_type_ids": tf.int32}, tf.int64),
+        (
+            {
+                "input_ids": tf.TensorShape([None]),
+                "attention_mask": tf.TensorShape([None]),
+                "token_type_ids": tf.TensorShape([None]),
+            },
+            tf.TensorShape([]),
+        ),
+    )
+
+def create_dataFrame(X,y):
+    dataFrame = pd.DataFrame(data = [X,y], index=['Body_of_Review','Label']).T
+    return dataFrame
